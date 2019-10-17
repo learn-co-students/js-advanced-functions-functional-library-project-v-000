ba89a823c1e10314721361813283b42a550f2581
Jonathan Luk <jluk87@gmail.com>
2019-10-17 22:52:41 +0000

    Done.

--- index.js
+++ index.js
@@ -4,16 +4,26 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      for (const i in collection) {
+        callback(collection[i]);
+      }
+      return collection;
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      const newCollection = [];
+      for (const i in collection) {
+        newCollection.push(callback(collection[i]));
+      }
+      return newCollection;
     },
 
-    reduce: function() {
-
+    reduce: function(collection, callback, acc) {
+      for (const i in collection) {
+        acc = callback(acc, collection[i], collection);
+      }
+      return acc;
     },
 
     functions: function() {
