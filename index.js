2aa4a346b9532caf87725feb8245997253092aa5
epic0448 <samuelogundiran@Yahoo.com>
2019-10-25 11:17:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { Certificate } from "crypto"
+// import { Certificate } from "crypto"
 
 const fi = (function() {
   return {
@@ -73,50 +73,85 @@ const fi = (function() {
        return acc
     },
 
-    // find: function (collections, predicate) {
-    //  let checkCollection = collections.find(collection => collection === predicate)
-    // },
+  //  Goal of this function is you want it tobe able to handle what every you throw at tit. 
+    find: function (collections, predicate) {
+
+      if (!(collections instanceof Array)) {
+        collections = Object.values(collections)
+      }
+      for (let i = 0; i < collections.length; i++){
+        if (predicate(collections[i]))
+          return collections[i]
+            
+        }
+         return undefined
+        
+    },
+     
+
+    filter: function (collection, predicate) {
+
+      let emptyArray = []
+
+      if (!(collection instanceof Array)) {
+        collection = Object.values(collection)
+      }
+      for (let i = 0; i < collection.length; i++)
+      
+    if (predicate(collection[i])) emptyArray.push(collection[i])
+      return emptyArray
+    },
 
 
-    // filter: function () {
+    size: function (collection) {
     
-    // },
-
-
-    // size: function () {
+      return (collection instanceof Array) ? collection.length : Object.keys(collection).length
     
-    // },
+    },
 
 
-    // first: function () {
+    first: function (collection, n = false) {
+      // n is false by default.
+      return n ? collection.slice(0,n) :  collection[0]
+    },
+
+
+    last: function (collection, n = false) {
+      //  you want it to show that last n value put in . So if you say N should be 2
+      //  it gonna only show the last 2 element in the collection
+      return n ? collection.slice(collection.length-n , collection.length) :  collection[collection.length-1]
+
+    },
+
+
+    compact: function (list) {
+      const noGood = new Set ([false, null, 0, "", undefined, NaN])
+      
+      return list.filter( obj => !noGood.has(obj));
+    },
+
+
+    sortby: function (collection, callback) {
+       
+      let newSort = [...collection]
+      return newSort.sort(function (a, b) {
+        // ranked in ascending order by the results of running each value through callback.
+        // The values from the original
+        // array should be retained within the sorted copy, just in ascending order.
+          return callback(a) - callback(b)
+        })
+    },
+
+
+
+    flatten: function () {
     
-    // },
+    },
 
 
-    // last: function () {
+    uniq: function () {
     
-    // },
-
-
-    // compact: function () {
-    
-    // },
-
-
-    // sortby: function () {
-    
-    // },
-
-
-
-    // flatten: function () {
-    
-    // },
-
-
-    // uniq: function () {
-    
-    // },
+    },
 
     functions: function() {
       
