45da29ac22e421de67fd733704b785802dce04d9
github username <github email address>
2020-01-03 06:58:50 -0800

    Done.

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, callback) {
 
+      if (Array.isArray(collection) === true) {
+        for (let i = 0; i < collection.length; i++) {
+          callback(collection[i]);
+        }
+      } else {
+        let object = Object.values(collection);
+        for (let i = 0; i < object.length; i++) {
+          callback(object[i]);
+        }
+      }
+      return collection;
     },
 
     map: function() {
