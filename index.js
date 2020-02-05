3fb0993358a837969833a4c31595504b2748cc1e
Sushi Chanrai <sushi@pathworks.co>
2020-02-05 17:12:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
+const alertFn = (function(element){
+ //Object.values(element) ? alert(Object.values(element)) : alert(element)
+  //!!element[key] ?  alert(Object.values(element)) : alert(element)
+  //alert(Object.values(element));
+ alert(element)
+  //for (let key in element) {
+  //alert(key, yourobject[key]);
+})()
+
+
 const fi = (function() {
   return {
     libraryMethod: function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(coll, alertFn) {
+      //for (let x = 0; x < coll.length; x++) {
+      //  alertFn(coll[x]);
+      //}
+      const newCollection = Object.values(coll)
+      for (element of newCollection) {
+        alertFn(element)
+      }
+      return coll;
     },
 
     map: function() {
