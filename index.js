f36dc7ebde19a35c1801ef3bee79011b67932a76
Sushi Chanrai <sushi@pathworks.co>
2020-02-05 21:38:50 -0500

    Done.

--- index.js
+++ index.js
@@ -14,14 +14,15 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function(coll, alertFn) {
+    each: function(coll, something) {
       //for (let x = 0; x < coll.length; x++) {
       //  alertFn(coll[x]);
       //}
-      const newCollection = Object.values(coll)
-      for (element of newCollection) {
-        alertFn(element)
-      }
+
+      const newCollection = (coll instanceof Array) ? coll.slice() : Object.values(coll)
+      // for (element of newCollection) {
+      //   something(element)
+      // }
       return coll;
     },
 
