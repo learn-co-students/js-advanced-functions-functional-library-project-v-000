245cc0821a949f96235c9efe102cc2e10118d281
William Barela <william.barela.dev@gmail.com>
2020-02-22 06:08:26 +0000

    Done.

--- index.js
+++ index.js
@@ -4,16 +4,40 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, f) {
+      const items = (collection instanceof Array)? collection : Object.values(collection);
+      
+      for(const item of items){
+        f(item);
+      }
+      
+      return collection;
     },
 
-    map: function() {
-
+    map: function(collection, f) {
+      const items = (collection instanceof Array) ? collection : Object.values(collection);
+      const newCollection = []
+      
+      for(const item of items){
+        newCollection.push(f(item));
+      }
+      
+      return newCollection;
     },
 
-    reduce: function() {
-
+    reduce: function(collection, f, acc) {
+      let items = collection;
+      
+      if (!acc){
+        acc = items[0];
+        items = items.slice(1)
+      }
+        
+      for(const item of items){
+        acc = f(acc, item, items);
+      }
+      
+      return acc;
     },
 
     functions: function() {
