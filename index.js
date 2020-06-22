77c3c346a645683305e52e6b0ae8a45170207c76
sjwakeman <swakeman@ca.rr.com>
2020-06-22 12:26:22 -0700

    Done.

--- index.js
+++ index.js
@@ -7,22 +7,36 @@ const fi = (function() {
     },
 
     each: function(collection, callback) {
-      let arrayCopy = (collection instanceof Array) ? collection.slice() : Object.values(collection)
-      // Check if collection is an Array 
-     
-      // Iterates over a collection of elements
+                      // Check if collection is an Array                  // collection is an Objecct
+      let arrayCopy = (collection instanceof Array) ? collection.slice() : Object.values(collection) 
+
+        // Iterates over a collection of elements
         for (let i = 0; i < arrayCopy.length; i++) {
-        //passing each element in turn to a callback function
-      
+          //passing each element in turn to a callback function
           callback(arrayCopy[i]) 
         } 
+      console.log("collection", collection)
+      // collection [ 1, 2, 3, 4 ]
+      // collection { one: 1, two: 2, three: 3, four: 4 }
+      // returns the original collection
       return collection
     },
 
-
-    // map: function() {
-
-    // },
+    map: function(collection, callback) {
+      // Produces a new array of values by mapping each value in collection through a transformation function (callback)
+      let newArray = !(collection instanceof Array) ? Object.values(collection) :  collection.slice()
+      // let newArray = (collection instanceof Array) ? collection.slice() : Object.values(collection) // Alternative
+      let newArrayResult = []
+      // iterate over collection of elements
+      for (let i = 0; i < newArray.length; i++) {
+        newArrayResult.push(callback(newArray[i]))    
+      }
+      // console.log("newArray", newArray)
+      // newArray [ 1, 2, 3, 4 ]
+      // console.log("newArrayResult", newArrayResult)
+      // newArrayResult [ 3, 6, 9, 12 ]
+      return newArrayResult
+    },
 
     // reduce: function() {
 
