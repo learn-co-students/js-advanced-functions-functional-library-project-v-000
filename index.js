3d37667b9b1cf861c1ea72e4b399b5dc8f5e4fa4
Eva Navarro <emanao@me.com>
2020-07-17 23:01:12 +0000

    each implementation

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, callback) {
+      const keys = Object.keys(collection);
 
+      for(const key of keys){
+        callback(collection[key],key,collection);
+      }
+      
+      return collection;
     },
 
     map: function() {
