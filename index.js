4193174a04f5f91de80ce31821cb32c1b1e55e58
ADogTheWarrior <coolfudge@gmail.com>
2020-09-24 10:59:02 -0600

    Done.

--- index.js
+++ index.js
@@ -4,22 +4,76 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, callback) {
+      if (collection instanceof Object){
+        let arrayOfValues = Object.values(collection)
+        for (const index in arrayOfValues){
+          callback(arrayOfValues[index])
+        }
+      }
+      else{
+        for(let i = 0; i < collection.length; i++){
+          callback(collection[i])
+        }
+      }
+      return collection
+    },
+
+    map: function(collection, callback) {
 
     },
 
-    map: function() {
+    reduce: function(collection, callback, acc) {
 
     },
 
-    reduce: function() {
+    find: function(collection, predicate) {
 
     },
 
-    functions: function() {
+    filter: function(collection, predicate) {
 
     },
 
+    size: function(collection) {
+
+    },
+
+    first: function(array, [n]) {
+
+    },
+
+    last: function(array, [n]) {
+
+    },
+
+    compact: function(array) {
+
+    },
+
+    sortBy: function(array, callback) {
+
+    },
+
+    flatten: function(array, [shallow]) {
+
+    },
+
+    uniq: function(array, [isSorted], [callback]) {
+
+    },
+
+    keys: function(object) {
+
+    },
+
+    values: function(object) {
+
+    },
+
+    functions: function(object) {
+
+    },
 
   }
 })()
