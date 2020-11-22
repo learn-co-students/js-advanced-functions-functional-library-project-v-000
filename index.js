6e2e68246011ebfdf1baae4716557dc89c13d1dc
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2020-11-22 11:33:16 -0500

    Done.

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ const fi = (function() {  //wrap entire library in IIFE
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function(arr, call) {
-           let i = 0 
-           let a = arr[i]
-           let length = arr.length
-            
-      let call = function(a, i, arr) { 
-
+    each: function(array1, callback) {
+    if (Array.isArray(array1)) {
+      for (let i = 0; i < array1.length; i++) {
+        let a = array1[i]
+        callback(a, i, array1)
       }
+    }
+    else {
+       let keys = Object.keys(array1)
+      for (let i = 0; i < keys.length; i++) {
+        let k = keys[i]
+        let v = array1[k]
+        callback(v, k, array1)
+      }
+    }
+    return array1 
+  },
 
-    },
 
     map: function() {
 
