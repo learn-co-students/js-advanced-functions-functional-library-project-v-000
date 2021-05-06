d3e5555a6236bb25203b16d92e8dfa2b70afab02
Laura Messa Travin <laura.travin@gmail.com>
2021-05-06 13:56:19 +0000

    Done.

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, callback) {
+           //ask if  collection is array
+           const valuesCollection = Array.isArray(collection) ?  collection.slice() : Object.values(collection)
+           for (let i = 0; i < valuesCollection.length; i++)
+           callback(valuesCollection[i])
 
+      return collection
     },
 
     map: function() {
